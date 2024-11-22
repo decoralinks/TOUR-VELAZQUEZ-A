f9a8c3db4a68bdaada01855f3c32aa4b70f41536
decoralinks <newsletter@decoralinks.com>
2024-11-22 21:13:26 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,395 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-a8_saln",
+      "name": "A8_Salón",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.016434804977501827,
+          "pitch": 0.6184438900222187,
+          "rotation": 0,
+          "target": "8-a9_comedor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-a1_entrada",
+      "name": "A1_entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.0553276305838768,
+        "pitch": 0.1842395633465621,
+        "fov": 1.44273126168384
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.02580810874062678,
+          "pitch": 0.5416204893920629,
+          "rotation": 1.5707963267948966,
+          "target": "3-a2_distribuidor1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-a3_distribuidor2",
+      "name": "A3_distribuidor2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.232310810770363,
+          "pitch": 0.8827103058016235,
+          "rotation": 0,
+          "target": "4-a4_dorm2"
+        },
+        {
+          "yaw": 0.7741412799042777,
+          "pitch": 0.792702237536707,
+          "rotation": 0.7853981633974483,
+          "target": "6-a6_bao"
+        },
+        {
+          "yaw": -0.00001584097544338192,
+          "pitch": 0.4750438891136941,
+          "rotation": 0,
+          "target": "8-a9_comedor"
+        },
+        {
+          "yaw": -0.8153408689620214,
+          "pitch": 0.7897708478264072,
+          "rotation": 4.71238898038469,
+          "target": "5-a5_dorm1"
+        },
+        {
+          "yaw": -3.073923915747125,
+          "pitch": 0.44741514241895075,
+          "rotation": 0,
+          "target": "3-a2_distribuidor1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-a2_distribuidor1",
+      "name": "A2_distribuidor1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5869840013640975,
+          "pitch": 0.5292009198047865,
+          "rotation": 12.566370614359176,
+          "target": "1-a1_entrada"
+        },
+        {
+          "yaw": -1.0667477074917784,
+          "pitch": 0.41657698377877495,
+          "rotation": 6.283185307179586,
+          "target": "2-a3_distribuidor2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-a4_dorm2",
+      "name": "A4_Dorm2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1253721335121671,
+          "pitch": 0.4414486451873181,
+          "rotation": 0,
+          "target": "2-a3_distribuidor2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-a5_dorm1",
+      "name": "A5_Dorm1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.127462344411537,
+          "pitch": 0.373758404199549,
+          "rotation": 0,
+          "target": "2-a3_distribuidor2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-a6_bao",
+      "name": "A6_Baño",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.979591724832435,
+          "pitch": 0.39183964478355904,
+          "rotation": 0,
+          "target": "2-a3_distribuidor2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-a7_cocina",
+      "name": "A7_Cocina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.056987876636553025,
+          "pitch": 0.4700201176303924,
+          "rotation": 0,
+          "target": "8-a9_comedor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-a9_comedor",
+      "name": "A9_Comedor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 1.4710580723345519,
+        "pitch": 0.28152877977547064,
+        "fov": 1.44273126168384
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5380882586784033,
+          "pitch": 0.5077066555195877,
+          "rotation": 0,
+          "target": "7-a7_cocina"
+        },
+        {
+          "yaw": 3.051460382695316,
+          "pitch": 0.5023456412038687,
+          "rotation": 0,
+          "target": "2-a3_distribuidor2"
+        },
+        {
+          "yaw": -1.4828143902861441,
+          "pitch": 0.7687922125999886,
+          "rotation": 0,
+          "target": "0-a8_saln"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "VELAZQUEZ A",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
